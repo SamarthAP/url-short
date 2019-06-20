84c971b2f19f2dd622adce3b02a72faf420490ce
Samarth Patel <samarth.patel.22@hotmail.com>
2019-06-20 15:06:08 -0400

    decodes url

--- api/main.go
+++ api/main.go
@@ -2,11 +2,31 @@ package main
 
 import (
 	f "fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 )
 
+func getShortURL(w http.ResponseWriter, r *http.Request) {
+	longurl, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal("Error reading body:", err)
+		http.Error(w, "can't ready body", http.StatusBadRequest)
+		return
+	}
+	decodedURL, err := url.QueryUnescape(string(longurl))
+	if err != nil {
+		log.Fatal("Error decoding url:", err)
+		http.Error(w, "can't decode url", http.StatusNotAcceptable)
+		return
+	}
+	w.Write([]byte(decodedURL))
+}
+
 func main() {
+	http.HandleFunc("/api/getshort/", getShortURL)
+
 	f.Println("Server running on port 9000")
 
 	err := http.ListenAndServe(":9000", nil)
