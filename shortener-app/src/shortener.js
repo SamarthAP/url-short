4fa8539421fc4573b74756e2a2a85d97b85d1620
Samarth Patel <samarth.patel.22@hotmail.com>
2019-06-27 10:37:11 -0400

    Edited styles and made POST to backend

--- shortener-app/src/shortener.js
+++ shortener-app/src/shortener.js
@@ -9,18 +9,37 @@ class Shortener extends React.Component {
             long: null,
             short: null
         };
+        this.onChangeHandler = this.onChangeHandler.bind(this);
+        this.onClickHandler = this.onClickHandler.bind(this);
+    }
+
+    onChangeHandler = event => {
+        this.setState({long: event.target.value});
+    }
+
+    onClickHandler = () => {
+        fetch('http://localhost:9000/api/getshort/', {
+            method: 'POST',
+            body: this.state.long
+        })
+        .then(data => {return data.text()})
+        .then(res => {
+            this.setState({short: res})
+        })
     }
 
     render() {
         return (
             <div className="container">
-                <input></input>
-                <button>Copy</button>
-                <div className="links">
-                    <p>Shortened Link</p>
-                    <p>Longer Link this is long</p>
+                <input onChange={this.onChangeHandler}></input>
+                <div className="flex-horizontal">
+                    <button className="left" onClick={this.onClickHandler}>Shorten</button>
+                    <p className="right">Longer link this is long</p>
                 </div>
-                
+                <div className="flex-horizontal">
+                    <button className="left">Copy</button>
+                    <p className="right">Short link this is short</p>
+                </div>
             </div>
         )
     }
